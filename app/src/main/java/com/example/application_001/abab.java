87618e36cd9987d0aec88979590ea44a16a4a76a
USER <1001lily@naver.com>
2022-11-16 20:21:15 +0900

    test 2022 11 16 20:20

---
+++ app/src/main/java/com/example/application_001/abab.java
@@ -0,0 +1,13 @@
+package com.example.application_001;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class abab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.abab);
+    }
+}
---
+++ app/src/main/java/com/example/application_001/activity_03_01.java
@@ -0,0 +1,14 @@
+package com.example.application_001;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_03_01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_0301);
+    }
+}
